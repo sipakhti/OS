06512bb8036c20e665a038a8b88d8c2fbb36de9b
Umer Mehmood <476061@gmail.com>
2022-03-27 02:45:00 +0500

    first commit

---
+++ testa41.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct human 
+{
+    int a;
+    char b;
+};
+
+void print(struct human obj)
+{
+    printf("%d %c\n",obj.a,obj.b);
+}
+
+int main(int argc, char*argv)
+{
+    //struct object runtime
+    struct human *obj =(struct human*)  malloc(10* sizeof(struct human));
+    obj[0].a = 200;
+    obj[0].b = 'z';
+    print(obj[0]);
+ /*   int a;
+    float p=10;
+    char c ='c';
+    printf("Enter the first value=");
+    scanf("%d",&a);
+
+    printf("Hell OS\n");
+    printf("int value = %d %f %c\n",a,p,c);
+    if(a<=30)
+    {
+        printf("Fail\n");
+    }
+    else
+    {
+        printf("pass\n");
+    }
+    int* p = (int*) malloc(10*sizeof(int));
+}
+*/
+    return 0;
+}
