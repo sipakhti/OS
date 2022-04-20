33beee756177bc962a503f9ca4584d735bc549b3
Umer Mehmood Khan Rana <476061@gmail.com>
2022-04-20 16:55:52 +0500

    refactored code to make pipes more readable

--- pipe.c
+++ pipe.c
@@ -3,9 +3,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
+#include<sys/wait.h>
 
 
-int main(int argc, char** argv)
+#define READ 0
+#define WRITE 1
+
+
+
+int main(int argc, char** cmdLineArgs)
 {
 
     pid_t pid_min, pid_sum;
@@ -25,22 +31,22 @@ int main(int argc, char** argv)
     }
     if (pid_min == 0) // child process for finding the min
     {
-        close(pipe_min[0]); // close read end
+        close(pipe_min[READ]); // close read end
 
         // run a program /home/sipakhti/OS/LinkedList.c using exec
 
-        int min = atoi(argv[1]);
+        int min = atoi(cmdLineArgs[1]);
         for (int i = 2; i < argc; i++)
         {
-            if (atoi(argv[i]) < min)
+            if (atoi(cmdLineArgs[i]) < min)
             {
-                min = atoi(argv[i]);
+                min = atoi(cmdLineArgs[i]);
             }
         }
 
-        write(pipe_min[1], &min, sizeof(int));
-        close(pipe_min[1]);
-        exit(0);
+        write(pipe_min[WRITE], &min, sizeof(int));
+        close(pipe_min[WRITE]);
+        return min;
     }
     else 
     {
@@ -52,26 +58,30 @@ int main(int argc, char** argv)
         }
         if (pid_sum == 0) // child process for finding the sum
         {
-            close(pipe_sum[0]); // close read end
+            close(pipe_sum[READ]); // close read end
 
             int sum = 0;
             for (int i = 1; i < argc; i++)
             {
-                sum += atoi(argv[i]);
+                sum += atoi(cmdLineArgs[i]);
             }
 
-            write(pipe_sum[1], &sum, sizeof(int));
-            close(pipe_sum[1]);
-            exit(0);
+            write(pipe_sum[WRITE], &sum, sizeof(int));
+            close(pipe_sum[WRITE]);
+            return sum;
         }
         else
         {
-            close(pipe_min[1]); // close write end
-            close(pipe_sum[1]); // close write end
+            close(pipe_min[WRITE]); // close write end
+            close(pipe_sum[WRITE]); // close write end
 
             int min, sum;
-            read(pipe_min[0], &min, sizeof(int));
-            read(pipe_sum[0], &sum, sizeof(int));
+            read(pipe_min[READ], &min, sizeof(int));
+            read(pipe_sum[READ], &sum, sizeof(int));
+
+
+            printf("MIN: %d\n", waitpid(pid_min, NULL, 0));
+            printf("SUM: %d\n", waitpid(pid_sum, NULL, 0));
 
             printf("min = %d, sum = %d\n", min, sum);
         }
